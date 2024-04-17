8451fc1c786c27939647fa6cba89a0d4f23f6d3a
Harpreet Sahota <sahota.singh.harpreet@gmail.com>
2024-04-16 20:09:09 -0500

    updating prompts

--- helpers/prompts.py
+++ helpers/prompts.py
@@ -1,36 +1,31 @@
-QUESTION_GEN_QUERY = """Your task is to write a factoid question given a context.
+QUESTION_GEN_PROMPT = """Your task is to write a question given a context. Your question must be in the form of an adult mentee seeking advice 
+from a trusted mentor. Formulate your question in the same style as questions users could ask in a search engine. Your question must be 
+answerable with a specific, concise piece of information from the context. 
 
-The questions you generate should be answerable with a specific, concise piece of factual information from the context.  Restrict the questions to the context information provided and ask the questions from the first-person perspective. 
+The context is below:
+----------------------
+{context}
+----------------------
 
-There is no need to address the thinker directly, they know you're speaking to them specifically. Ask the question directly and as succinctly as possible. Your question MUST NOT mention something like "according to the passage" or "context".
-
+Your question MUST be short, clear, and based on the essence of the context. DO NOT use any qualifiers, relative clauses, or introductory modifiers.  
+Keep your question short and to the point. Ask your question using the first person perspective, in the form of a student seeking advice from a trusted mentor.
 """
 
-TEXT_QUESTION_STR = """ Your task is to write a factoid question given a context.
+ANSWER_GEN_PROMPT = """You're a trusted mentor to an adult mentee. Your mentee is seeking advice in the form of a question.
 
-Context information is below.
+Below is your mentee's question:
 
----------------------
-{context_str}
----------------------
+----------------------
+{question}
+----------------------
 
-You're seeking wisdom and advice. Given the context information, generate questions that seek actionable advice or practical tips that you can apply in your own life. 
+You've previously done some thinking and writing on the exact question your mentee has asked. Your answer MUST be actionable
+yet concise. It must be based on your thinking and writing, which is below:
 
-The questions you generate should be answerable with a specific, concise piece of factual information from the context.  Restrict the questions to the context information provided and ask the questions from the first-person perspective. 
+----------------------
+{context}
+----------------------
 
-There is no need to address the thinker directly, they know you're speaking to them specifically. Ask the question directly and as succinctly as possible. Your question MUST NOT mention something like "according to the passage" or "context".
-"""
-
-TEXT_QA_STRING = """Context information is below.
-
----------------------
-{context_str}
----------------------
-
-You are the author of the context. Given the context information provide advice that answers the query. Use only the given context to provide advice. 
-
-When providing advice, speak directly to the asker and use the "you" voice (second-person perspective). There is no need to identify yourself in your response, the asker knows who you are. 
-
-Query: {query_str}
-Answer:
+DO NOT use any qualifiers, relative clauses, or introductory modifiers in your answer. Provide your answer question using the second person
+perspective, speaking directly to your mentee, in the form of a trusted mentor providing actionable advice.
 """
